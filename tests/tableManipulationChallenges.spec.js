3942664c06d13d765de932712b127b5be8cba408
Gabriela Papin <gabrielapapin20@gmail.com>
2022-05-26 22:15:55 -0300

    desafio 24concluindo

--- tests/tableManipulationChallenges.spec.js
+++ tests/tableManipulationChallenges.spec.js
@@ -114,7 +114,7 @@ describe('Desafios de manipulação de tabelas', () => {
       });
     });
 
-    describe.only("24 - Atualize os dados da coluna 'discount' da tabela 'order_details' para 45, onde o valor na coluna 'unit_price' seja maior que 10.0000 e o id seja um número entre 30 e 40", () => {
+    describe("24 - Atualize os dados da coluna 'discount' da tabela 'order_details' para 45, onde o valor na coluna 'unit_price' seja maior que 10.0000 e o id seja um número entre 30 e 40", () => {
       it('Verifica o desafio24', async () => {
         const challengeQuery = readFileSync('desafio24.sql', 'utf8').trim();
 
